ea7705d74f7052d6e04aea5436df1d53381ce436
fayjullah haque <fayjullah.h.emon@gmail.com>
2026-08-27 14:09:32 +0600

    Two Sum

---
+++ 1_Two_Sum.js
@@ -0,0 +1,28 @@
+var twoSum = function (nums, target){
+    let obj = {};
+    for(const num of nums){
+        let remaining = target-num;
+        if(obj.hasOwnProperty(remaining)){
+            let flag;
+            for(let i = 0; i<nums.length;i++){
+                if(nums[i]===remaining){
+                    flag = i;
+                }
+            }
+            return [flag,nums.indexOf(num)]
+        }else{
+            obj[num] = nums.indexOf(num);
+            // console.log(obj)
+        }
+    }
+}
+console.log(twoSum([3,3],6));
+console.log(twoSum([3,2,4],6));
+// console.log([3,4,2].indexOf(4));
+// let abc = {
+//     '3':0,
+// }
+// if(Object.keys(abc).includes('3')){
+//     abc['3'] = abc['3']+1;
+// }
+// console.log(abc['3'])
